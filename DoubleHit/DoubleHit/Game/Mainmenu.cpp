6df5c69fd47a55d361b4dcf769c812b0b52dc463
wonnn-kim <won.kim@digipen.edu>
2024-06-13 11:15:00 +0900

    no option

--- DoubleHit/DoubleHit/Game/Mainmenu.cpp
+++ DoubleHit/DoubleHit/Game/Mainmenu.cpp
@@ -30,7 +30,7 @@ void Mainmenu::Load() {
     index = 0;
     main_background = Engine::GetTextureManager().Load("Assets/main.png");
     side_texture = Engine::GetFont(static_cast<int>(Fonts::Basic)).PrintToTexture("GameStart", 0xF4F4F4FF);
-    space_texture = Engine::GetFont(static_cast<int>(Fonts::Basic)).PrintToTexture("Option", 0xF4F4F4FF);
+    space_texture = Engine::GetFont(static_cast<int>(Fonts::Basic)).PrintToTexture("Start", 0xF4F4F4FF);
     credit_texture = Engine::GetFont(static_cast<int>(Fonts::Basic)).PrintToTexture("Credit", 0xF4F4F4FF);
     exit_texture = Engine::GetFont(static_cast<int>(Fonts::Basic)).PrintToTexture("Quit", 0xF4F4F4FF);
     
@@ -41,6 +41,7 @@ void Mainmenu::Update([[maybe_unused]] double dt) {
     //GetGSComponent<UI>()->Update(dt, -20);
 
     //start
+    /*
     if (GetMouseX() >= Engine::GetWindow().GetSize().x / 2 - side_texture->GetSize().x / 2
         && GetMouseX() <= Engine::GetWindow().GetSize().x / 2 + side_texture->GetSize().x / 2
         && GetMouseY() >= 400 - side_texture->GetSize().y / 2
@@ -54,20 +55,20 @@ void Mainmenu::Update([[maybe_unused]] double dt) {
     else {
         side_texture = Engine::GetFont(static_cast<int>(Fonts::Basic)).PrintToTexture("Game Start", 0xF4F4F4FF);
 
-    }
+    }*/
     //option
     if (GetMouseX() >= Engine::GetWindow().GetSize().x / 2 - space_texture->GetSize().x / 2
         && GetMouseX() <= Engine::GetWindow().GetSize().x / 2 + space_texture->GetSize().x / 2
         && GetMouseY() >= 480 - space_texture->GetSize().y / 2
         && GetMouseY() <= 480 + space_texture->GetSize().y / 2) {
-        space_texture = Engine::GetFont(static_cast<int>(Fonts::Basic)).PrintToTexture("Option", 0x3b5dc9ff);
+        space_texture = Engine::GetFont(static_cast<int>(Fonts::Basic)).PrintToTexture("Start", 0x3b5dc9ff);
 
         if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
             Engine::GetGameStateManager().SetNextGameState(static_cast<int>(States::Mode1));
         }
     }
     else {
-        space_texture = Engine::GetFont(static_cast<int>(Fonts::Basic)).PrintToTexture("Option", 0xF4F4F4FF);
+        space_texture = Engine::GetFont(static_cast<int>(Fonts::Basic)).PrintToTexture("Start", 0xF4F4F4FF);
 
     }
     //credit
@@ -123,7 +124,7 @@ void Mainmenu::Draw() {
     Engine::GetWindow().Clear(0x000000);
 
     main_background->Draw(Math::TranslationMatrix(Math::ivec2{ 0,0 }));
-    side_texture->Draw(Math::TranslationMatrix(Math::ivec2{ Engine::GetWindow().GetSize().x / 2 - side_texture->GetSize().x / 2, Engine::GetWindow().GetSize().y - side_texture->GetSize().y - 400 }));
+    //side_texture->Draw(Math::TranslationMatrix(Math::ivec2{ Engine::GetWindow().GetSize().x / 2 - side_texture->GetSize().x / 2, Engine::GetWindow().GetSize().y - side_texture->GetSize().y - 400 }));
     space_texture->Draw(Math::TranslationMatrix(Math::ivec2{ Engine::GetWindow().GetSize().x / 2 - space_texture->GetSize().x / 2, Engine::GetWindow().GetSize().y - space_texture->GetSize().y - 480 }));
     credit_texture->Draw(Math::TranslationMatrix(Math::ivec2{ Engine::GetWindow().GetSize().x / 2 - credit_texture->GetSize().x / 2, Engine::GetWindow().GetSize().y - credit_texture->GetSize().y - 560 }));
     exit_texture->Draw(Math::TranslationMatrix(Math::ivec2{ Engine::GetWindow().GetSize().x / 2 - exit_texture->GetSize().x / 2, Engine::GetWindow().GetSize().y - exit_texture->GetSize().y - 640 }));
